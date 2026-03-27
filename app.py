6fea9b3bc9a094d87c667b3343f035c8b3328587
24f2008859 <24f2008859@ds.study.iitm.ac.in>
2026-03-27 12:10:59 +0530

    Milestone-PPA DB-Relationship

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy 
-from models import db, User, Job, Application 
+from models import db, Admin, Company, Student, Job, Application, Placement
 
 app = Flask(__name__)
 app.secret_key = "placeme_secret"
@@ -13,39 +13,72 @@ def home():
     if request.method == "POST":
         email = request.form.get("email")
         password = request.form.get("password")
-        user = User.query.filter_by(email=email).first()
-        if user and user.password == password:
-           return redirect(url_for('home'))
+        admin = Admin.query.filter_by(username=email).first()
+        student = Student.query.filter_by(email=email).first()
+        company = Company.query.filter_by(email=email).first()
+        if admin and admin.password == password:
+            return redirect(url_for('admin_dashboard'))
+        elif student and student.password == password:
+           return redirect(url_for('student_dashboard'))
+        elif company and company.password == password:
+            if company.is_approved:
+                return redirect(url_for('company_dashboard'))
+            else:
+                flash("Waiting for admin approval", "warning")
         else:
             flash("Invalid credentials")
     return render_template("login.html")
 
 
 
+
 @app.route("/register", methods = ["GET", "POST"])
 def register():
     if request.method == "POST":
+        role = request.form.get("role")
         email = request.form.get("email")
         password = request.form.get("password")
         name = request.form.get("name")
-        role = request.form.get("role")
         department = request.form.get("department")
-        existing_user = User.query.filter_by(email=email).first()
-        if existing_user:
+        existing_user1 = Student.query.filter_by(email=email).first()
+        existing_user2 = Company.query.filter_by(email=email).first()
+        if existing_user1 or existing_user2:
             flash("Email already registered", "danger" )
         else:
-            new_user = User(name = name, email = email, password = password, role = role, department = department)
-            db.session.add(new_user)
-            db.session.commit()
-            flash("successful", "success")
-            return redirect(url_for('home'))
+            if role == 'student':
+                new_user = Student(name = name, email = email, password = password, department = department)
+                db.session.add(new_user)
+                db.session.commit()
+                flash("Registration successful", "success")
+                return redirect(url_for('home'))
+            elif role == 'recruiter':
+                website = request.form.get("website")
+                new_user = Company(name = name, email = email, password = password, website= website)
+                db.session.add(new_user)
+                db.session.commit()
+                flash("Registration submitted! wait for Admin approval.", "success")
+                return redirect(url_for('home'))
     return render_template("register.html")
 
+@app.route("/admin_dashboard")
+def admin_dashboard():
+    return render_template("admin_dashboard.html")
+
+@app.route("/student_dashboard")
+def student_dashboard():
+    return render_template("student_dashboard.html")
+
+@app.route("/company_dashboard" )
+def company_dashboard():
+    return render_template("company_dashboard.html")
 
 
 with app.app_context():
     db.create_all()
-
+if not Admin.query.first():
+    admin = Admin(username="admin", password="admin123")
+    db.session.add(admin)
+    db.session.commit()
 
 if __name__ == "__main__":
     app.run(debug=True)
--- models.py
+++ models.py
@@ -2,24 +2,45 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Admin(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(100), nullable = False, unique = True) 
+    password = db.Column(db.String(200), nullable = False)
+
+
+class Company(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(100), nullable = False)
     email = db.Column(db.String(100), nullable = False, unique = True)
     password = db.Column(db.String(200), nullable = False)
-    role  = db.Column(db.String(20), nullable = False) 
-    department = db.Column(db.String(100), nullable = True)
+    website = db.Column(db.String(200), nullable = True)
+    is_approved = db.Column(db.Boolean, default = False)
 
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100), nullable = False)
+    email = db.Column(db.String(100), nullable = False, unique = True)
+    password = db.Column(db.String(200), nullable = False)
+    department = db.Column(db.String(100), nullable = False)
 
 class Job(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(100), nullable = False)
     description = db.Column(db.Text, nullable = False)
-    company = db.Column(db.String(100), nullable = False)
-    recruiter_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    company_id = db.Column(db.Integer, db.ForeignKey('company.id'))
+    company = db.relationship('Company', backref='jobs')
 
 class Application(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    student_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    student_id = db.Column(db.Integer, db.ForeignKey('student.id'))
     job_id = db.Column(db.Integer, db.ForeignKey('job.id'))
     status = db.Column(db.String(50), default = "applied")
+    student = db.relationship('Student', backref='applications')
+    job = db.relationship('Job', backref='applications')
+
+class Placement(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    application_id = db.Column(db.Integer, db.ForeignKey('application.id'))
+    salary = db.Column(db.String(100), nullable = True)
+    joining_date = db.Column(db.String(100), nullable = True)
+    application = db.relationship('Application', backref='placement')
